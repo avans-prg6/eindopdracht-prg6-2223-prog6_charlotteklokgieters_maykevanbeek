b0c996ce15f1ba5225e46bca30ab1a957c4e94fc
Charlotte Klokgieters <clr.klokgieters@student.avans.nl>
2022-12-13 14:19:46 +0100

    Started views and models and controllers for santa and the kids

---
+++ Santa_WishList/Controllers/KidController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Santa_WishList.Models;
+using System.Reflection;
+
+namespace Santa_WishList.Controllers
+{
+	public class KidController : Controller
+	{
+		public IActionResult Index()
+		{
+			KidViewModel model = new KidViewModel();
+
+			return View("Index", model);
+		}
+
+		public IActionResult PersonalInfo(int age, Niceness niceness, string example)
+		{
+			KidViewModel model = new KidViewModel();
+			model.Age = age;
+			model.Niceness = niceness;
+
+			if (niceness != Niceness.Naughty)
+			{
+				model.NicenessExample = example;
+			}
+			
+			return View("Wishlist", model);
+		}
+
+		//TODO enum verplaatsen
+		public enum Niceness
+		{
+			Very_Nice,
+			A_Little,
+			Naughty
+		}
+	}
+}
---
+++ Santa_WishList/Models/KidViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Santa_WishList.Controllers.KidController;
+
+namespace Santa_WishList.Models
+{
+	public class KidViewModel
+	{
+		public string Name { get; set; }
+		[Required(ErrorMessage = "Je moet je leeftijd invullen!")]
+		[RegularExpression("^[0-9]*$", ErrorMessage = "Je kan geen tekens anders dan getallen invullen voor je leeftijd!")]
+		[CustomValidation(typeof(ValidationMethods), "NoNegativesOrZero")]
+		public int Age { get; set; }
+		public Niceness Niceness { get; set; }
+		[CustomValidation(typeof(ValidationMethods), "GiveNicenessExample")]
+		public string NicenessExample { get; set; }
+	}
+
+	//TODO verplaats class
+	public class ValidationMethods
+	{
+		public static ValidationResult NoNegativesOrZero(int value, ValidationContext context)
+		{
+			if (value > 0)
+			{
+				return ValidationResult.Success;
+			}
+			else
+			{
+				return new ValidationResult(
+					string.Format("Je kan hier geen negatieve getallen of 0 invullen!", context.MemberName),
+					new List<string>() { context.MemberName });
+			}
+		}
+
+		public static ValidationResult GiveNicenessExample(string input, ValidationContext context)
+		{
+			if (input != null)
+			{
+				return ValidationResult.Success;
+			}
+			else
+			{
+				return new ValidationResult(
+					string.Format("Je moet hier een voorbeeld geven waarom je braaf bent geweest!", context.MemberName),
+					new List<string>() { context.MemberName });
+			}
+		}
+	}
+}
