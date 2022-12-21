1f2f64de380da6fc883289b5e65827d203d5b7d5
Charlotte Klokgieters <clr.klokgieters@student.avans.nl>
2022-12-21 12:36:20 +0100

    Update kid views & controller

--- Santa_WishList/Controllers/KidController.cs
+++ Santa_WishList/Controllers/KidController.cs
@@ -12,13 +12,13 @@ using static Santa_WishList.Controllers.KidController;
 
 namespace Santa_WishList.Controllers
 {
-    [Authorize(Roles = "Child")]
-    public class KidController : Controller
+	//[Authorize(Roles = "Child")] TODO
+	public class KidController : Controller
 	{
 		readonly GiftRepository giftRepository;
 
-		public KidController(GiftRepository injectedGiftRepository) 
-		{ 
+		public KidController(GiftRepository injectedGiftRepository)
+		{
 			giftRepository = injectedGiftRepository;
 		}
 
@@ -32,67 +32,119 @@ namespace Santa_WishList.Controllers
 			return View("Index", model);
 		}
 
-		public IActionResult PersonalInfo(Kid model) //TODO
+		public IActionResult PersonalInfo(Kid model)
 		{
 			if (!ModelState.IsValid)
 			{
-                List<string> errors = ModelState.Values.SelectMany(ms => ms.Errors).Select(err => err.ErrorMessage).ToList();
+				List<string> errors = ModelState.Values.SelectMany(ms => ms.Errors).Select(err => err.ErrorMessage).ToList();
 
 				ViewBag.Errors = errors;
 				return Index();
 			}
 
 			model.PossibleGifts = giftRepository.GetPossibleGifts();
-			model.ChosenGifts = new List<Gift>();
 			return View("Wishlist", model);
 		}
 
+
 		public IActionResult ChoosingGifts(Kid model, List<GiftViewModel> chosenGifts)
 		{
-			Console.WriteLine(chosenGifts.Count());
-			Console.WriteLine("1:" + chosenGifts.ElementAt(0).IsChecked);
+			model.PossibleGifts = giftRepository.GetPossibleGifts();
+			model.ChosenGifts = new List<string>();
+
+			if (!ModelState.IsValid)
+			{
+				List<string> errors = ModelState.Values.SelectMany(ms => ms.Errors).Select(err => err.ErrorMessage).ToList();
+
+				ViewBag.Errors = errors;
+				return View("Wishlist", model);
+			}
+
 			if (model.Other != null)
 			{
-				string[] otherGifts = model.Other.Split(", "); //TODO
+				string[] otherGifts;
+				if (model.Other.Contains(", "))
+				{
+					 otherGifts = model.Other.Split(", ");
+				}
+				else if (model.Other.Contains(","))
+				{
+					otherGifts = model.Other.Split(",");
+				}
+				else
+				{
+					otherGifts = new string[1];
+					otherGifts[0] = model.Other;
+				}
+				
 				foreach (string otherGift in otherGifts)
 				{
 					foreach (Gift gift in model.PossibleGifts)
 					{
 						if (gift.Name.ToLower() == otherGift.ToLower())
 						{
-							//Error, gift is in list
+							ValidationResult result = new ValidationResult(
+								errorMessage: "Een cadeautje dat je hebt gekozen staat al tussen de cadeautjes hierboven waar je uit kan kiezen!",
+								memberNames: new[] { "Other" }
+								);
+
+							List<string> errors = ModelState.Values.SelectMany(ms => ms.Errors).Select(err => err.ErrorMessage).ToList();
+							errors.Add(result.ErrorMessage);
+
+							ViewBag.Errors = errors;
+							return View("Wishlist", model);
+						}
+						else
+						{
+							model.ChosenGifts.Add(otherGift);
+							break;
 						}
 					}
 				}
-
-				foreach (string otherGift in otherGifts)
+			}
+			
+			foreach (Gift gift in model.PossibleGifts)
+			{
+				foreach (GiftViewModel gvm in chosenGifts)
 				{
-					Gift newGift = new Gift();
-					newGift.Name = otherGift;
-					//TODO category???
-
-					model.ChosenGifts.Add(newGift);
+					if (gvm.IsChecked && gvm.Gift == gift.Name)
+					{
+						model.ChosenGifts.Add(gift.Name); 
+					}
 				}
-			}			
+			}
 
 			return View("Confirmation", model);
 		}
 
-		public IActionResult Confirm()
+		public IActionResult Confirm(Kid model)
 		{
-			//TODO send list
+			model.PossibleGifts = giftRepository.GetPossibleGifts();
+
+			WishList list = new WishList();
+			list.Name = model.Name;
+			list.Wanted = new List<Gift>();
+
+			foreach (Gift gift in model.PossibleGifts)
+			{
+				foreach (string chosenGift in model.ChosenGifts)
+				{
+					if (chosenGift == gift.Name)
+					{
+						list.Wanted.Add(gift);
+					}
+				}
+			}
+
+			giftRepository.SendWishList(list);
 			//TODO log out, can't log in again
 			return View();
 		}
 
-		public IActionResult BackToWishlist(Kid previous) { return View("Wishlist", previous); }
-
-		//TODO enum verplaatsen
-		public enum Niceness
+		public IActionResult BackToWishlist(Kid model)
 		{
-			Very_Nice,
-			A_Little,
-			Naughty
-		}
+			model.PossibleGifts = giftRepository.GetPossibleGifts();
+			return View("Wishlist", model);
+		}		
 	}
 }
--- Santa_WishList/Models/Viewmodels/GiftViewModel.cs
+++ Santa_WishList/Models/Viewmodels/GiftViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using SantasWishlist.Domain;
+using System.ComponentModel.DataAnnotations;
 
 namespace Santa_WishList.Models.Viewmodels
 {
     public class GiftViewModel
     {
         public bool IsChecked { get; set; }
-        public Gift Gift { get; set; }
+
+        public string Gift { get; set; }
     }
 }
