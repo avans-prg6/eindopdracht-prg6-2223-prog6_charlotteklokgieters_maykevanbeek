b1381ba4ddf905e6b3d3a829260d6f1bcfbf61c5
Mayke <mtmbeek@avans.nl>
2022-12-22 10:22:44 +0100

    general

---
+++ LogicLayer/General/General.cs
@@ -0,0 +1,26 @@
+﻿namespace LogicLayer.General
+{
+    public class General
+    {
+        public string[] SplitNames(string names)
+        {
+            string[] kids;
+
+            if (names.Contains(", "))
+            {
+                kids = names.Split(", ");
+            }
+            else if (names.Contains(","))
+            {
+                kids = names.Split(",");
+            }
+            else
+            {
+                kids = new string[1];
+                kids[0] = names;
+            }
+
+            return kids;
+        }
+    }
+}
